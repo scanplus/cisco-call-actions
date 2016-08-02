f0b340c7f8db7b53dd32076cd5cfe014d7da6097
Tobias Genannt <t.genannt@scanplus.de>
2016-08-02 11:35:07 +0200

    Trust proxy in openshift

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', true)
 
 app.use(logger('short'));
 app.use(bodyParser.json());
